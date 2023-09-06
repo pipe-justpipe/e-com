770688699706bce646eb13b746651479238e2a53
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-09-06 21:26:56 +0100

    commit

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -1,2 +1,41 @@
-let btn = document.querySelector('.button')
-console.log(btn)
+const reduce = document.querySelector('.negative');
+const increase = document.querySelector('.positive');
+const black = document.querySelector('.black'); 
+let count = 0
+
+increase.addEventListener('click',function increment(){
+    count++;
+    black.textContent = count
+} );
+console.log(count);
+
+reduce.addEventListener('click',function reduce(){
+    console.log(count);
+    count--;
+    black.textContent = count
+} ); 
+
+// function increment(){
+//     // count = count +1
+
+//     count+= 25
+//     countEl.textContent = count
+// }
+
+
+// function save (){
+// // 1. Grab the save-el paragrah and store it in a variable called saveEl
+// // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+// // 3. Render the variable in the saveEl using innerText/////
+    
+// let countStr = count + " - "
+// // NB: Make sure to not delete the existing content of the paragraph
+// saveEl.textContent += countStr
+
+// countEl.textContent = 0
+// count=0
+
+// console.log(count)
+// }
+
+
